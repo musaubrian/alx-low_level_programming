144d99fa12896d62bf9b75f8258f9200db18a2cd
musaubrian <musaubrian15@gmail.com>
2022-03-10 11:06:59 -0800

    Programme using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf
+ * Author: Ernest Musau
+ */
+
+#include <stdio.h>
+
+/**
+ * main - uses the printf function to output string,
+ * followed by a new line
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	return (0);
+}
