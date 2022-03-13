0fad3e8968218ea98a37a85b0f05e5c461ee9aa6
musaubrian <musaubrian15@gmail.com>
2022-03-13 07:38:29 -0700

    Negative or positive
    Chacks if an integer is negative, positive or equals to zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+/*more headers goes her*/
+
+/**
+ * main - generates a random number
+ * Checks if it is greater than 0,
+ * equals to zero
+ * or is less than zero
+ * Return: o
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
