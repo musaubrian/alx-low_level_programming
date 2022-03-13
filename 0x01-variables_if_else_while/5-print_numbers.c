6da773f611d9d31c65a5d35668520ada43845b00
musaubrian <musaubrian15@gmail.com>
2022-03-13 11:14:35 -0700

    Print base 10 numbbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers to base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb < 10; numb++)
+	{
+		printf("%d", numb);
+	}
+
+	return (0);
+}
