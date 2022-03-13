4781f4812bc8136bc10f04245f1fb8590244a28c
musaubrian <musaubrian15@gmail.com>
2022-03-13 08:38:36 -0700

    Correct syntax

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	char a;
 
-	for (a = 'a'; a <= 'z': a++)
+	for (a = 'a'; a <= 'z'; a++)
 	{
 		putchar(a);
 	}
 
-	for (a = 'A'; a <= 'Z': a++)
+	for (a = 'A'; a <= 'Z'; a++)
 	{
 		putchar(a);
 	}
