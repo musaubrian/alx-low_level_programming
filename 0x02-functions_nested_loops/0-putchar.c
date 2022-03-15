18e3189d3540a9d5ccd814666d0ff2719a818bc2
musaubrian <musaubrian15@gmail.com>
2022-03-15 03:54:13 -0700

    using main.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
+#include "main.c"
 
 /**
- * main - Prints "_putchar"
- * followed by a new line
- *
+ * main - uses _putchar to print "_putchar"
+ * followed by anew line
  * Return: 0
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar('\n');
 
 	return (0);
 }
