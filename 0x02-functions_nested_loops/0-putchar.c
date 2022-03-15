2ccb6e56cb7ba71a82c4d28f928618c4c3c6ad6e
musaubrian <musaubrian15@gmail.com>
2022-03-15 04:49:02 -0700

    header file iincluded

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * main - uses _putchar to print "_putchar"
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	
+	return (0);
+}
