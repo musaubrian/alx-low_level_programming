307f5731dd82dc9a23d98f15d833d99e97fd1625
musaubrian <musaubrian15@gmail.com>
2022-03-16 04:31:49 -0700

    Print all natural numbers to 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of 3 or 5 until 1024
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
