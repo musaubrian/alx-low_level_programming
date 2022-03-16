cc16c4a66ca20cd04bacb105fc0587b74741d302
musaubrian <musaubrian15@gmail.com>
2022-03-16 04:54:51 -0700

    print sum of fibonacci series

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints sum of even values,
+ * in fibonacci sequence not exceding 4,000,000
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	unsigned long i = 0, j = 1, sum;
+	float total;
+
+	while (1)
+	{
+		sum = i + j;
+		if (sum > 4000000)
+			break;
+
+		if ((sum %2) == 0)
+			total += sum;
+
+		i = j;
+		j = sum;
+	}
+
+	printf("%.0f\n", total);
+
+	return (0);
+}
