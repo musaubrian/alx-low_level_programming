24ec6a1a5411c5c50001094496c119d8c5b86c36
musaubrian <musaubrian15@gmail.com>
2022-03-17 04:49:53 -0700

    Started debugging all over

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - checks if n is positive or negative
+ *
+ * @n: integer being checked
+ *
+ * Return: 0.
  */
 
 int main(void)
