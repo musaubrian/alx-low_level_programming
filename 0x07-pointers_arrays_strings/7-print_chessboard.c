37309968250d4ac55ce8af37a2bee687b9de2798
musaubrian <musaubrian15@gmail.com>
2022-03-28 04:11:29 -0700

    print chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chess board
+ *
+ * @a: pointer for chessboard printing
+ */
+
+void print_chessboard(char(*a)[8])
+{
+	int row, col;
+
+	for (row = 0; a[row][7]; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+
+		_putchar('\n');
+	}
+}
