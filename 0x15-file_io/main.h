2581ead577dc963f67881c75817d29134522a529
musaubrian <musaubrian15@gmail.com>
2022-05-03 08:25:59 -0700

    update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+ssize_t read/-textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *make_buff(char *file);
+void close_fd(int fd);
 
 #endif
