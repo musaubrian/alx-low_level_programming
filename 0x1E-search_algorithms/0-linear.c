7d62a50ec4b506338ce1554a8efab03334f3f180
musaubrian <musaubrian15@gmail.com>
2022-10-05 09:29:55 +0300

    chore(format): follow betty codestyle

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,17 +13,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-  size_t idx;
+	size_t idx;
 
-  if (array == NULL) {
-    return (-1);
-  }
-  
-  for (idx = 0; idx < size; idx++) {
-    printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
-    if (value == array[idx]) {
-      return (idx);
-    }
-  }
-  return (-1);
+	if (array == NULL)
+		return (-1);
+
+	for (idx = 0; idx < size; idx++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
+		if (value == array[idx])
+			return (idx);
+	}
+
+	return (-1);
 }
